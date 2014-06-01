3f6270085e7accd1cc20f05047566b5b6e484522
Dustin Peaire <dmpeaire@gmail.com>
2014-06-01 10:40:27 -0400

    Added multiple places to scrape.py

--- scrape.py
+++ scrape.py
@@ -4,58 +4,81 @@ from app.models import db, Issue, Reporter
 import requests, json
 
 per_page = 10
-page = 1
 
-while True:
-  response = requests.get('https://seeclickfix.com/api/v2/issues?page=%s&per_page=%i&place_url=raleigh' % (page,per_page))
-  data = response.json()
-  next_page = data['metadata']['pagination'].get('next_page')
-  if next_page == None: break
+places = [
+  'raleigh-six-forks-corridor', 
+  'six-forks',
+  'raleigh',
+  'falls-of-neuse',
+  'glenwood_wake',
+  'five-points_wake',
+  'mordecai',
+  'wake-county',
+  'wade_wake',
+  'raleigh_downtown',
+  'north-central_wake',
+  'hillsborough_wake',
+  'east-raleigh',
+  'central_wake',
+  'north_wake',
+  'south-central_wake'
+]
 
-  for item in data['issues']:
 
-    if not item['reporter']['id']: continue
+for place in places:
 
-    issue = Issue(
-      id=item.get('id'), 
-      status=item.get('status'),
-      description=item.get('description'),
-      url=item.get('url'),
-      summary=item.get('summary'),
-      html_url=item.get('html_url'),
-      comment_url=item.get('comment_url'),
-      shortened_url=item.get('shortened_url'),
-      address=item.get('address'),
-      lat=item.get('lat'),
-      lng=item.get('lng'),
-      closed_at=item.get('closed_at'),
-      acknowledged_at=item.get('acknowledged_at'),
-      created_at=item.get('created_at'),
-      updated_at=item.get('updated_at')
-    )
+  page = 1
 
-    reporter = Reporter(
-      id=item['reporter'].get('id'),
-      witty_title=item['reporter'].get('witty_title'),
-      civic_points=item['reporter'].get('civic_points'),
-      name=item['reporter'].get('name')
-    )
+  while True:
+    response = requests.get('https://seeclickfix.com/api/v2/issues?page=%s&per_page=%i&place_url=%s' % (page,per_page,place))
+    data = response.json()
+    next_page = data['metadata']['pagination'].get('next_page')
+    if next_page == None: break
 
-    issue.reporter_id = reporter.id
+    for item in data['issues']:
 
-    print issue.id, issue.description
-    print reporter.id, reporter.name
+      if not item['reporter']['id']: continue
 
-    try:
-      db.session.add(issue)
-      db.session.commit()
-    except IntegrityError:
-      db.session.rollback()
+      issue = Issue(
+        id=item.get('id'), 
+        status=item.get('status'),
+        description=item.get('description'),
+        url=item.get('url'),
+        summary=item.get('summary'),
+        html_url=item.get('html_url'),
+        comment_url=item.get('comment_url'),
+        shortened_url=item.get('shortened_url'),
+        address=item.get('address'),
+        lat=item.get('lat'),
+        lng=item.get('lng'),
+        closed_at=item.get('closed_at'),
+        acknowledged_at=item.get('acknowledged_at'),
+        created_at=item.get('created_at'),
+        updated_at=item.get('updated_at')
+      )
 
-    try:
-      db.session.add(reporter)
-      db.session.commit()
-    except IntegrityError:
-      db.session.rollback()
+      reporter = Reporter(
+        id=item['reporter'].get('id'),
+        witty_title=item['reporter'].get('witty_title'),
+        civic_points=item['reporter'].get('civic_points'),
+        name=item['reporter'].get('name')
+      )
 
-  page += 1
+      issue.reporter_id = reporter.id
+
+      print issue.id, issue.description
+      print reporter.id, reporter.name
+
+      try:
+        db.session.add(issue)
+        db.session.commit()
+      except IntegrityError:
+        db.session.rollback()
+
+      try:
+        db.session.add(reporter)
+        db.session.commit()
+      except IntegrityError:
+        db.session.rollback()
+
+    page += 1
